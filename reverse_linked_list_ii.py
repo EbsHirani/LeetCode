3abe171b3264d1f22332d682f1ad3435c0960e7b
EbsHirani <hiraniebrahim13@gmail.com>
2023-12-31 19:37:35 -0800

    Added onepass solution

--- reverse_linked_list_ii.py
+++ reverse_linked_list_ii.py
@@ -39,4 +39,47 @@ class Solution:
         
         return head
 
+###One Pass Solution
+    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        count = 1
+        if left == right or not head or not head.next:
+            return head
+        curr = head
+
+        while count<left-1:
+            curr = curr.next
+            count+=1
+        leftStart = curr
+        if left!=1:
+            leftNode = leftStart.next
+        else:
+            leftNode=leftStart
+        prev = leftNode
+        while count<right:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr=temp
+            count+=1
+        rightNode = curr
+        rightEnd = curr.next
+        rightNode.next = prev
+        leftNode.next = rightEnd
+        if left ==1:
+            return rightNode
+        leftStart.next = rightNode
+        
+        
+        return head
+
+
+
+
 
