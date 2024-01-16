da575cce18e3e03d93884deba962cc5fd08f8b42
EbsHirani <hiraniebrahim13@gmail.com>
2024-01-15 16:11:57 -0800

    stack or 2 pointer

--- trapping_rainwater.py
+++ trapping_rainwater.py
@@ -13,3 +13,28 @@ class Solution:
                     ans += dist*ht
             stack.append(i)
         return ans
+
+#2 pointer solution
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        ans = 0
+        left, right = 0, len(height)-1
+        leftmax = rightmax = 0
+
+        while left<right:
+            if height[left]< height[right]:
+                if height[left]<leftmax:
+                    ans+= leftmax-height[left]
+                else:
+                    leftmax = height[left]
+                left+=1
+            else:
+                if height[right]<rightmax:
+                    ans+= rightmax - height[right]
+                else:
+                    rightmax = height[right]
+                right-=1
+        return ans
+                    
+
+        return ans
